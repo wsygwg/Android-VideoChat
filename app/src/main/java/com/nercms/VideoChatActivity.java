c2a074f3ad68f798c96c08375c4deadfaf8bcc04
wsygwg <496292631@qq.com>
2018-10-31 15:33:40 +0800

    修改摄像头的设置，使其符合当前手机

--- app/src/main/java/com/nercms/VideoChatActivity.java
+++ app/src/main/java/com/nercms/VideoChatActivity.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.List;
 
 /**
  * 视频聊天活动
@@ -64,7 +65,7 @@ public class VideoChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_chat);
         remote_ip = getIntent().getStringExtra("remote_ip");
-        remote_port=getIntent().getIntExtra("remote_port",8080);
+        remote_port = getIntent().getIntExtra("remote_port", 8080);
         initView();
 
     }
@@ -85,11 +86,17 @@ public class VideoChatActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                doStart();
+                try {
+                    doStart();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }, 1000);
 
     }
+
+    private static final String TAG = VideoChatActivity.class.getSimpleName();
 
     /**
      * 开启 接受 发送rtp线程  开启本地摄像头
@@ -130,16 +137,22 @@ public class VideoChatActivity extends AppCompatActivity {
         if (mCamera == null) {
 
             //摄像头设置，预览视频
-            mCamera = Camera.open(1); //实例化摄像头类对象  0为后置 1为前置
+            mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT); //实例化摄像头类对象  0为后置 1为前置
             Camera.Parameters p = mCamera.getParameters(); //将摄像头参数传入p中
+            printPreviewSizes(p);
+            printSupportedStringList("SupportedFlashModes", p.getSupportedFlashModes());
+            printSupportedStringList("SupportedFocusModes", p.getSupportedFocusModes());
+            printSupportedIntegerList("SupportedPreviewFrameRates", p.getSupportedPreviewFrameRates());
+            printSupportedStringList("SupportedSceneModes", p.getSupportedSceneModes());
+            printSupportedStringList("SupportedWhiteBalance", p.getSupportedWhiteBalance());
+            printSupportedIntegerList("SupportedPreviewFormats", p.getSupportedPreviewFormats());
+
             p.setFlashMode("off");
             p.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_AUTO);
             p.setSceneMode(Camera.Parameters.SCENE_MODE_AUTO);
-            p.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-            //p.setPreviewFormat(PixelFormat.YCbCr_420_SP); //设置预览视频的格式
-            p.setPreviewFormat(ImageFormat.NV21);
+            p.setFocusMode(Camera.Parameters.FOCUS_MODE_FIXED);
+            p.setPreviewFormat(ImageFormat.NV21);//0x11 = 17
             p.setPreviewSize(352, 288); //设置预览视频的尺寸，CIF格式352×288
-            //p.setPreviewSize(800, 600);
             p.setPreviewFrameRate(15); //设置预览的帧率，15帧/秒
             mCamera.setParameters(p); //设置参数
             byte[] rawBuf = new byte[1400];
@@ -159,54 +172,82 @@ public class VideoChatActivity extends AppCompatActivity {
         }
     }
 
+    private void printSupportedStringList(String title, List<String> list) {
+        Log.e(TAG, title + "###########################################");
+        int i = 1;
+        for (String s : list) {
+            Log.e(TAG, title + " " + i + " " + s);
+            i++;
+        }
+    }
+
+    private void printSupportedIntegerList(String title, List<Integer> list) {
+        Log.e(TAG, title + "###########################################");
+        int i = 1;
+        for (int s : list) {
+            Log.e(TAG, title + " " + i + " " + s);
+            i++;
+        }
+    }
+
+    private void printPreviewSizes(Camera.Parameters p) {
+        List<Camera.Size> supList = p.getSupportedPreviewSizes();
+        for (Camera.Size size : supList) {
+            Log.e(TAG, "PreView width: " + size.width + ",height: " + size.height);
+        }
+    }
+
     //mCamera回调的类
     class Callback implements Camera.PreviewCallback {
         @Override
         public void onPreviewFrame(byte[] frame, Camera camera) {
+            try {
+                if (encoder_handle != -1) {
+                    //底层函数，返回包的数目，返回包的大小存储在数组packetSize中，返回码流在stream中
+                    send_packetNum = encode.EncoderOneFrame(encoder_handle, -1, frame, send_stream, send_packetSize);
+                    Log.d("log", "原始数据大小：" + frame.length + "  转码后数据大小：" + send_stream.length);
+                    if (send_packetNum > 0) {
 
-
-            if (encoder_handle != -1) {
-                //底层函数，返回包的数目，返回包的大小存储在数组packetSize中，返回码流在stream中
-                send_packetNum = encode.EncoderOneFrame(encoder_handle, -1, frame, send_stream, send_packetSize);
-                Log.d("log", "原始数据大小：" + frame.length + "  转码后数据大小：" + send_stream.length);
-                if (send_packetNum > 0) {
-
-                    //通过RTP协议发送帧
-                    final int[] pos = {0}; //从码流头部开始取
-                    final long timestamp = System.currentTimeMillis(); //设定时间戳
-                    /**
-                     * 因为可能传输数据过大 会将一次数据分割成好几段来传输
-                     * 接受方 根据序列号和结束符 来将这些数据拼接成完整数据
-                     */
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            int sequence = 0; //初始化序列号
-                            for (int i = 0; i < send_packetNum; i++) {
-
-                                rtp_send_packet.setPayloadType(2);//定义负载类型，视频为2
-                                rtp_send_packet.setMarker(i == send_packetNum - 1 ? true : false); //是否是最后一个RTP包
-                                rtp_send_packet.setSequenceNumber(sequence++); //序列号依次加1
-                                rtp_send_packet.setTimestamp(timestamp); //时间戳
-                                //Log.d("log", "序列号:" + sequence + " 时间：" + timestamp);
-                                rtp_send_packet.setPayloadLength(send_packetSize[i]); //包的长度，packetSize[i]+头文件
-                                //从码流stream的pos处开始复制，从socketBuffer的第12个字节开始粘贴，packetSize为粘贴的长度
-                                System.arraycopy(send_stream, pos[0], socket_send_Buffer, 12, send_packetSize[i]); //把一个包存在socketBuffer中
-                                pos[0] += send_packetSize[i]; //重定义下次开始复制的位置
-                                //rtp_packet.setPayload(socketBuffer, rtp_packet.getLength());
-                                //  Log.d("log", "序列号:" + sequence + " bMark:" + rtp_packet.hasMarker() + " packetSize:" + packetSize[i] + " tPayloadType:2" + " timestamp:" + timestamp);
+                        //通过RTP协议发送帧
+                        final int[] pos = {0}; //从码流头部开始取
+                        final long timestamp = System.currentTimeMillis(); //设定时间戳
+                        /**
+                         * 因为可能传输数据过大 会将一次数据分割成好几段来传输
+                         * 接受方 根据序列号和结束符 来将这些数据拼接成完整数据
+                         */
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
                                 try {
-                                    rtp_socket.send(rtp_send_packet);
-                                } catch (IOException e) {
+                                    int sequence = 0; //初始化序列号
+                                    for (int i = 0; i < send_packetNum; i++) {
+
+                                        rtp_send_packet.setPayloadType(2);//定义负载类型，视频为2
+                                        rtp_send_packet.setMarker(i == send_packetNum - 1 ? true : false); //是否是最后一个RTP包
+                                        rtp_send_packet.setSequenceNumber(sequence++); //序列号依次加1
+                                        rtp_send_packet.setTimestamp(timestamp); //时间戳
+                                        //Log.d("log", "序列号:" + sequence + " 时间：" + timestamp);
+                                        rtp_send_packet.setPayloadLength(send_packetSize[i]); //包的长度，packetSize[i]+头文件
+                                        //从码流stream的pos处开始复制，从socketBuffer的第12个字节开始粘贴，packetSize为粘贴的长度
+                                        System.arraycopy(send_stream, pos[0], socket_send_Buffer, 12, send_packetSize[i]); //把一个包存在socketBuffer中
+                                        pos[0] += send_packetSize[i]; //重定义下次开始复制的位置
+                                        //rtp_packet.setPayload(socketBuffer, rtp_packet.getLength());
+                                        //  Log.d("log", "序列号:" + sequence + " bMark:" + rtp_packet.hasMarker() + " packetSize:" + packetSize[i] + " tPayloadType:2" + " timestamp:" + timestamp);
+                                        try {
+                                            rtp_socket.send(rtp_send_packet);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                } catch (Exception e) {
                                     e.printStackTrace();
                                 }
-
                             }
-                        }
-                    }).start();
-
-
+                        }).start();
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -216,43 +257,47 @@ public class VideoChatActivity extends AppCompatActivity {
      */
     class DecoderThread extends Thread {
         public void run() {
-            while (isRunning) {
-                try {
-                    rtp_socket.receive(rtp_receive_packet); //接收一个包
-                } catch (IOException e) {
-                    e.printStackTrace();
+            try {
+                while (isRunning) {
+                    try {
+                        rtp_socket.receive(rtp_receive_packet); //接收一个包
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    int packetSize = rtp_receive_packet.getPayloadLength(); //获取包的大小
+
+                    if (packetSize <= 0)
+                        continue;
+                    if (rtp_receive_packet.getPayloadType() != 2) //确认负载类型为2
+                        continue;
+                    System.arraycopy(socket_receive_Buffer, 12, buffer, 0, packetSize); //socketBuffer->buffer
+                    int sequence = rtp_receive_packet.getSequenceNumber(); //获取序列号
+                    long timestamp = rtp_receive_packet.getTimestamp(); //获取时间戳
+                    int bMark = rtp_receive_packet.hasMarker() == true ? 1 : 0; //是否是最后一个包
+                    int frmSize = decode.PackH264Frame(decoder_handle, buffer, packetSize, bMark, (int) timestamp, sequence, frmbuf); //packer=拼帧器，frmbuf=帧缓存
+                    Log.d("log", "序列号:" + sequence + " bMark:" + bMark + " packetSize:" + packetSize + " PayloadType:" + rtp_receive_packet.getPayloadType() + " timestamp:" + timestamp + " frmSize:" + frmSize);
+                    if (frmSize <= 0)
+                        continue;
+
+                    decode.DecoderNal(frmbuf, frmSize, view.mPixel);//解码后的图像存在mPixel中
+
+                    //Log.d("log","序列号:"+sequence+" 包大小："+packetSize+" 时间："+timestamp+"  frmbuf[30]:"+frmbuf[30]);
+                    view.postInvalidate();
                 }
-                int packetSize = rtp_receive_packet.getPayloadLength(); //获取包的大小
 
-                if (packetSize <= 0)
-                    continue;
-                if (rtp_receive_packet.getPayloadType() != 2) //确认负载类型为2
-                    continue;
-                System.arraycopy(socket_receive_Buffer, 12, buffer, 0, packetSize); //socketBuffer->buffer
-                int sequence = rtp_receive_packet.getSequenceNumber(); //获取序列号
-                long timestamp = rtp_receive_packet.getTimestamp(); //获取时间戳
-                int bMark = rtp_receive_packet.hasMarker() == true ? 1 : 0; //是否是最后一个包
-                int frmSize = decode.PackH264Frame(decoder_handle, buffer, packetSize, bMark, (int) timestamp, sequence, frmbuf); //packer=拼帧器，frmbuf=帧缓存
-                Log.d("log", "序列号:" + sequence + " bMark:" + bMark + " packetSize:" + packetSize + " PayloadType:" + rtp_receive_packet.getPayloadType() + " timestamp:" + timestamp + " frmSize:" + frmSize);
-                if (frmSize <= 0)
-                    continue;
-
-                decode.DecoderNal(frmbuf, frmSize, view.mPixel);//解码后的图像存在mPixel中
-
-                //Log.d("log","序列号:"+sequence+" 包大小："+packetSize+" 时间："+timestamp+"  frmbuf[30]:"+frmbuf[30]);
-                view.postInvalidate();
+                //关闭
+                if (decoder_handle != 0) {
+                    decode.DestroyH264Packer(decoder_handle);
+                    decoder_handle = 0;
+                }
+                if (rtp_socket != null) {
+                    rtp_socket.close();
+                    rtp_socket = null;
+                }
+                decode.DestoryDecoder();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            //关闭
-            if (decoder_handle != 0) {
-                decode.DestroyH264Packer(decoder_handle);
-                decoder_handle = 0;
-            }
-            if (rtp_socket != null) {
-                rtp_socket.close();
-                rtp_socket = null;
-            }
-            decode.DestoryDecoder();
         }
     }
 
